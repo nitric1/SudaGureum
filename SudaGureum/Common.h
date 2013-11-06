869b24264475f28ec0a9fb53a6e6772d831adba0
Nitric <mail@nitric.me>
2013-11-06 17:53:50 +0900

    Chat in specific channel.

--- SudaGureum/Common.h
+++ SudaGureum/Common.h
@@ -1,15 +1,29 @@
 #pragma once
 
-#include "Batang/Common.h"
+#define NOMINMAX
+#define _VARIADIC_MAX 10
+#define _SCL_SECURE_NO_WARNINGS
+
+#define WINVER 0x0600 // Windows Vista
+#define _WIN32_WINDOWS 0x0600
+#define _WIN32_WINNT 0x0600
+#define _WIN32_IE 0x0800
+#define NTDDI_VERSION 0x06000100
+
+// All inclusion listing is dictionary order; Ordering is case insensitive.
+// Standard C/C++ library & boost library inclusion
 
 #include <atomic>
 #include <deque>
 #include <iostream>
+#include <memory>
 #include <mutex>
+#include <set>
 #include <sstream>
+#include <thread>
+#include <unordered_map>
 
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/assign/list_of.hpp>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
@@ -17,3 +31,11 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
 #include <boost/shared_ptr.hpp>
+
+using std::max; using std::min;
+
+// External library inclusion
+
+// Other common inclusion
+
+#include "Batang/Types.h"
--- SudaGureum/SudaGureum.cpp
+++ SudaGureum/SudaGureum.cpp
@@ -1,5 +1,7 @@
 #include "Common.h"
 
+#include "Batang/Utility.h"
+
 #include "SudaGureum.h"
 
 #include "IrcClient.h"
@@ -10,14 +12,36 @@ namespace SudaGureum
     {
         IrcClientPool pool;
 
-        auto client1 = pool.connect("irc.ozinger.org", 6667, boost::assign::list_of("SudaGureum1")("SudaGureum2"));
+        auto client1 = pool.connect("altirc.ozinger.org", 80, boost::assign::list_of("SudaGureum1")("SudaGureum2"));
 
         pool.run(4);
 
-        Sleep(100);
+        // Sleep(100);
 
-        auto client2 = pool.connect("irc.ozinger.org", 6667, boost::assign::list_of("SudaGureum1")("SudaGureum2"));
+        // auto client2 = pool.connect("irc.ozinger.org", 6667, boost::assign::list_of("SudaGureum1")("SudaGureum2"));
 
+        // pool.join();
+
+        while(std::cin)
+        {
+            std::string line;
+            std::getline(std::cin, line);
+
+            boost::algorithm::trim(line);
+
+            if(line.empty())
+            {
+                break;
+            }
+
+            size_t wlen = static_cast<size_t>(MultiByteToWideChar(CP_ACP, 0, line.c_str(), -1, nullptr, 0));
+            std::vector<wchar_t> wbuf(wlen);
+            MultiByteToWideChar(CP_ACP, 0, line.c_str(), -1, wbuf.data(), static_cast<int>(wlen));
+
+            client1.lock()->privmsg("#zvuc", Batang::encodeUTF8(wbuf.data()));
+        }
+
+        pool.closeAll();
         pool.join();
     }
 }
