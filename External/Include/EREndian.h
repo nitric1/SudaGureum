be72d6bece0d22aef30a07e4e3315cf83520be18
LEE Wondong <wdlee91@gmail.com>
2013-11-13 01:31:29 +0900

    Untabify

--- External/Include/EREndian.h
+++ External/Include/EREndian.h
@@ -5,217 +5,219 @@
 class EREndian
 {
 private:
-	/// Private and unimplemented constructor.
-	/// @author Wondong LEE
-	EREndian();
+    /// Private and unimplemented constructor.
+    /// @author Wondong LEE
+    EREndian();
 
-	/// Private and unimplemented destructor.
-	/// @author Wondong LEE
-	~EREndian();
+    /// Private and unimplemented destructor.
+    /// @author Wondong LEE
+    ~EREndian();
 
 public:
-	/// Swaps a 16-bit integer's lowest 8-bit and highest 8-bit.
-	/// @author Wondong LEE
-	/// @param v The value to swap.
-	/// @return Swapped value.
-	static uint16_t swap(uint16_t v)
-	{ return (v << 8) | (v >> 8); }
+    /// Swaps a 16-bit integer's lowest 8-bit and highest 8-bit.
+    /// @author Wondong LEE
+    /// @param v The value to swap.
+    /// @return Swapped value.
+    static uint16_t swap(uint16_t v)
+    { return (v << 8) | (v >> 8); }
 
-	/// Swaps a 32-bit integer's lowest 8-bit and highest 8-bit and each of middle 8-bit's.
-	/// @author Wondong LEE
-	/// @param v The value to swap.
-	/// @return Swapped value.
-	static uint32_t swap(uint32_t v)
-	{ return (v << 24) | ((v & 0xFF00u) << 8) | ((v & 0xFF0000u) >> 8) | (v >> 24); }
+    /// Swaps a 32-bit integer's lowest 8-bit and highest 8-bit and each of middle 8-bit's.
+    /// @author Wondong LEE
+    /// @param v The value to swap.
+    /// @return Swapped value.
+    static uint32_t swap(uint32_t v)
+    { return (v << 24) | ((v & 0xFF00u) << 8) | ((v & 0xFF0000u) >> 8) | (v >> 24); }
 
-	/// Swaps a 64-bit integer's each low and high 8-bit's.
-	/// @author Wondong LEE
-	/// @param v The value to swap.
-	/// @return Swapped value.
-	static uint64_t swap(uint64_t v)
-	{ return (v << 56) | ((v & 0xFF00ull) << 40) | ((v & 0xFF0000ull) << 24) |
-			 ((v & 0xFF000000ull) << 8) | ((v & 0xFF00000000ull) >> 8) |
-			 ((v & 0xFF0000000000ull) >> 24) | ((v & 0xFF000000000000ull) >> 40) |
-			 (v >> 56); }
+    /// Swaps a 64-bit integer's each low and high 8-bit's.
+    /// @author Wondong LEE
+    /// @param v The value to swap.
+    /// @return Swapped value.
+    static uint64_t swap(uint64_t v)
+    {
+        return (v << 56) | ((v & 0xFF00ull) << 40) | ((v & 0xFF0000ull) << 24) |
+               ((v & 0xFF000000ull) << 8) | ((v & 0xFF00000000ull) >> 8) |
+               ((v & 0xFF0000000000ull) >> 24) | ((v & 0xFF000000000000ull) >> 40) |
+               (v >> 56);
+    }
 
 #ifdef BOOST_BIG_ENDIAN
-	/// Converts a 16-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t N2L(uint16_t v) { return swap(v); }
-	
-	/// Converts a 32-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t N2L(uint32_t v) { return swap(v); }
+    /// Converts a 16-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t N2L(uint16_t v) { return swap(v); }
+    
+    /// Converts a 32-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t N2L(uint32_t v) { return swap(v); }
 
-	/// Converts a 64-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t N2L(uint64_t v) { return swap(v); }
-	
-	/// Converts a 16-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t N2B(uint16_t v) { return v; }
+    /// Converts a 64-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t N2L(uint64_t v) { return swap(v); }
+    
+    /// Converts a 16-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t N2B(uint16_t v) { return v; }
 
-	/// Converts a 32-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t N2B(uint32_t v) { return v; }
+    /// Converts a 32-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t N2B(uint32_t v) { return v; }
 
-	/// Converts a 64-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t N2B(uint64_t v) { return v; }
+    /// Converts a 64-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t N2B(uint64_t v) { return v; }
 #else
-	/// Converts a 16-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t N2L(uint16_t v) { return v; }
+    /// Converts a 16-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t N2L(uint16_t v) { return v; }
 
-	/// Converts a 32-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t N2L(uint32_t v) { return v; }
+    /// Converts a 32-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t N2L(uint32_t v) { return v; }
 
-	/// Converts a 64-bit unsigned integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t N2L(uint64_t v) { return v; }
+    /// Converts a 64-bit unsigned integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t N2L(uint64_t v) { return v; }
 
-	/// Converts a 16-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t N2B(uint16_t v) { return swap(v); }
+    /// Converts a 16-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t N2B(uint16_t v) { return swap(v); }
 
-	/// Converts a 32-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t N2B(uint32_t v) { return swap(v); }
+    /// Converts a 32-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t N2B(uint32_t v) { return swap(v); }
 
-	/// Converts a 64-bit unsigned integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t N2B(uint64_t v) { return swap(v); }
+    /// Converts a 64-bit unsigned integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t N2B(uint64_t v) { return swap(v); }
 #endif
-	/// Converts a 16-bit unsigned integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t L2N(uint16_t v) { return N2L(v); }
+    /// Converts a 16-bit unsigned integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t L2N(uint16_t v) { return N2L(v); }
 
-	/// Converts a 32-bit unsigned integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t L2N(uint32_t v) { return N2L(v); }
+    /// Converts a 32-bit unsigned integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t L2N(uint32_t v) { return N2L(v); }
 
-	/// Converts a 64-bit unsigned integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t L2N(uint64_t v) { return N2L(v); }
+    /// Converts a 64-bit unsigned integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t L2N(uint64_t v) { return N2L(v); }
 
-	/// Converts a 16-bit unsigned integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint16_t B2N(uint16_t v) { return N2B(v); }
+    /// Converts a 16-bit unsigned integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint16_t B2N(uint16_t v) { return N2B(v); }
 
-	/// Converts a 32-bit unsigned integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint32_t B2N(uint32_t v) { return N2B(v); }
+    /// Converts a 32-bit unsigned integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint32_t B2N(uint32_t v) { return N2B(v); }
 
-	/// Converts a 64-bit unsigned integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static uint64_t B2N(uint64_t v) { return N2B(v); }
+    /// Converts a 64-bit unsigned integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static uint64_t B2N(uint64_t v) { return N2B(v); }
 
-	/// Converts a 16-bit signed integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int16_t N2L(int16_t v) { return static_cast<int16_t>(N2L(static_cast<uint16_t>(v))); }
+    /// Converts a 16-bit signed integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int16_t N2L(int16_t v) { return static_cast<int16_t>(N2L(static_cast<uint16_t>(v))); }
 
-	/// Converts a 32-bit signed integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int32_t N2L(int32_t v) { return static_cast<int32_t>(N2L(static_cast<uint32_t>(v))); }
+    /// Converts a 32-bit signed integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int32_t N2L(int32_t v) { return static_cast<int32_t>(N2L(static_cast<uint32_t>(v))); }
 
-	/// Converts a 64-bit signed integer from native endian to little endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int64_t N2L(int64_t v) { return static_cast<int64_t>(N2L(static_cast<uint64_t>(v))); }
+    /// Converts a 64-bit signed integer from native endian to little endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int64_t N2L(int64_t v) { return static_cast<int64_t>(N2L(static_cast<uint64_t>(v))); }
 
-	/// Converts a 16-bit signed integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int16_t N2B(int16_t v) { return static_cast<int16_t>(N2B(static_cast<uint16_t>(v))); }
+    /// Converts a 16-bit signed integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int16_t N2B(int16_t v) { return static_cast<int16_t>(N2B(static_cast<uint16_t>(v))); }
 
-	/// Converts a 32-bit signed integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int32_t N2B(int32_t v) { return static_cast<int32_t>(N2B(static_cast<uint32_t>(v))); }
+    /// Converts a 32-bit signed integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int32_t N2B(int32_t v) { return static_cast<int32_t>(N2B(static_cast<uint32_t>(v))); }
 
-	/// Converts a 64-bit signed integer from native endian to big endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int64_t N2B(int64_t v) { return static_cast<int64_t>(N2B(static_cast<uint64_t>(v))); }
+    /// Converts a 64-bit signed integer from native endian to big endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int64_t N2B(int64_t v) { return static_cast<int64_t>(N2B(static_cast<uint64_t>(v))); }
 
-	/// Converts a 16-bit signed integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int16_t L2N(int16_t v) { return static_cast<int16_t>(L2N(static_cast<uint16_t>(v))); }
+    /// Converts a 16-bit signed integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int16_t L2N(int16_t v) { return static_cast<int16_t>(L2N(static_cast<uint16_t>(v))); }
 
-	/// Converts a 32-bit signed integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int32_t L2N(int32_t v) { return static_cast<int32_t>(L2N(static_cast<uint32_t>(v))); }
+    /// Converts a 32-bit signed integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int32_t L2N(int32_t v) { return static_cast<int32_t>(L2N(static_cast<uint32_t>(v))); }
 
-	/// Converts a 64-bit signed integer from little endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int64_t L2N(int64_t v) { return static_cast<int64_t>(L2N(static_cast<uint64_t>(v))); }
+    /// Converts a 64-bit signed integer from little endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int64_t L2N(int64_t v) { return static_cast<int64_t>(L2N(static_cast<uint64_t>(v))); }
 
-	/// Converts a 16-bit signed integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int16_t B2N(int16_t v) { return static_cast<int16_t>(B2N(static_cast<uint16_t>(v))); }
+    /// Converts a 16-bit signed integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int16_t B2N(int16_t v) { return static_cast<int16_t>(B2N(static_cast<uint16_t>(v))); }
 
-	/// Converts a 32-bit signed integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int32_t B2N(int32_t v) { return static_cast<int32_t>(B2N(static_cast<uint32_t>(v))); }
+    /// Converts a 32-bit signed integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int32_t B2N(int32_t v) { return static_cast<int32_t>(B2N(static_cast<uint32_t>(v))); }
 
-	/// Converts a 64-bit signed integer from big endian to native endian.
-	/// @author Wondong LEE
-	/// @param v The value to convert.
-	/// @return Converted value.
-	static int64_t B2N(int64_t v) { return static_cast<int64_t>(B2N(static_cast<uint64_t>(v))); }
+    /// Converts a 64-bit signed integer from big endian to native endian.
+    /// @author Wondong LEE
+    /// @param v The value to convert.
+    /// @return Converted value.
+    static int64_t B2N(int64_t v) { return static_cast<int64_t>(B2N(static_cast<uint64_t>(v))); }
 };
