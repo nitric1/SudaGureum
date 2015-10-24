a7a493d0d89c3e27f58bd85a0156360c3711f0ff
Nitric <mail@nitric.me>
2015-10-25 04:47:41 +0900

    Add a description of use cases of each spdlog logger level.

--- SudaGureum/Log.h
+++ SudaGureum/Log.h
@@ -4,6 +4,16 @@
 
 namespace SudaGureum
 {
+    /*
+     * Meaning of logger levels:
+     *   info: not harmful; just for lower-level contextual information, or really just for printing
+     *   trace: not harmful; just for higher-level contextual information
+     *   debug: info's or traces only for debug mode
+     *   alert: recovable errors including wrong data input
+     *   error: non-recovable errors
+     */
+
+    // Logger wrapper class which exposes interface returning the `spdlog::logger` instance.
     class Log : public Singleton<Log>
     {
     public:
