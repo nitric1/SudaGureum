15ebc03ec6107a0ef36663a454544a66336a7ab6
Nitric <mail@nitric.me>
2023-09-24 02:47:27 +0900

    Replace boost::thread_specific_ptr to C++11 thread_local.

--- SudaGureum/Singleton.h
+++ SudaGureum/Singleton.h
@@ -19,19 +19,11 @@ namespace SudaGureum
     template<typename T>
     class SingletonLocal // thread-local (using TLS)
     {
-    private:
-        static void deleter(T *ptr)
-        {
-            delete ptr;
-        }
-
     public:
         static T &instance()
         {
-            static boost::thread_specific_ptr<T> ptr(deleter);
-            if(ptr.get() == nullptr)
-                ptr.reset(new T);
-            return *ptr;
+            thread_local static T inst;
+            return inst;
         }
 
     protected:
